0639dbc2ef3377de2b6f327d6e156934f6dc7af5
Maksym Vdovenko <jackjackjack1376@gmail.com>
2023-08-26 15:52:40 +0900

    finishing

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,8 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_app/components/button.dart';
 import 'package:restaurant_app/models/shop.dart';
 import 'package:restaurant_app/theme/colors.dart';
 
@@ -9,8 +12,10 @@ class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   //remover
-  void removeFromCart(){
+  void removeFromCart(Food food, BuildContext context) {
+    final shop = context.read<Shop>();
 
+    shop.removeFromCart(food);
   }
 
   @override
@@ -21,44 +26,60 @@ class CartPage extends StatelessWidget {
           title: const Text('My Cart'),
           backgroundColor: primaryColor,
         ),
-        body: ListView.builder(
-          itemCount: value.cart.length,
-          itemBuilder: (context, index) {
-            //get food
-            final Food food = value.cart[index];
-            // food name
-            final String foodName = food.name;
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.cart.length,
+                itemBuilder: (context, index) {
+                  //get food
+                  final Food food = value.cart[index];
+                  // food name
+                  final String foodName = food.name;
 
-            //food price
-            final String foodPrice = food.price;
-            //return
-            return Container(
-              decoration: BoxDecoration(
-                color: secondaryColor,
+                  //food price
+                  final String foodPrice = food.price;
+                  //return
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    margin: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                    child: ListTile(
+                      title: Text(
+                        foodName,
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        foodPrice,
+                        style: TextStyle(
+                            color: Colors.grey[200],
+                            fontWeight: FontWeight.bold),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () => removeFromCart(food, context),
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.grey[300],
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
-              margin: const EdgeInsets.only(left: 20 , top: 20 , right: 20),
-              child: ListTile(
-                title: Text(foodName,
-                style: const TextStyle(
-                  color: Colors.white, 
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-                subtitle: Text(foodPrice ,
-                style:  TextStyle(
-                  color: Colors.grey[200], 
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-                trailing: IconButton(
-                  onPressed: removeFromCart, 
-                  icon: Icon(Icons.delete,
-                  color:Colors.grey[300],
-                  ),
-                  ),
+            ),
+
+            //pay now button
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: MyButton(
+                text: "Pay Now",
+                onTap: () {},
               ),
-            );
-          },
+            )
+          ],
         ),
       ),
     );
